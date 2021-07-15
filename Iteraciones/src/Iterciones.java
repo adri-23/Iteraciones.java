cc5a03fe85c67871d9d0a66882295847adc28376
adri-23 <adrianabermudez1023@gmail.com>
2021-07-15 17:58:24 -0500

    iteraciones

---
+++ Iteraciones/src/Iterciones.java
@@ -0,0 +1,15 @@
+
+public class Iterciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String frutas[] = {"mango","pera","manzana","uva"};
+		
+		//forEach en java
+		for(String fruta: frutas) {
+			System.out.println(fruta);
+		}
+	}
+
+}
